3dc2ad9a85293c630eb360157659eec517113d09
Kris Kuiper <kris@kriskuiper.nl>
2019-09-14 17:06:54 +0200

    Changed licence

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
  * sFire Framework
  *
  * @link      https://sfire.nl
- * @copyright Copyright (c) 2014-2018 sFire Framework.
+ * @copyright Copyright (c) 2014-2019 sFire Framework.
  * @license   http://sfire.nl/license BSD 3-CLAUSE LICENSE
  */
 
